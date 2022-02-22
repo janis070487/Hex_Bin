728b084bb74d75fdd451c6432e75771cd7c9fa8c
janis070487 <janis070487@gmail.com>
2022-02-22 22:28:14 +0200

    v0.12

--- Hex Bin/Program.cs
+++ Hex Bin/Program.cs
@@ -6,10 +6,27 @@ namespace Hex_Bin
     {
         static void Main(string[] args)
         {
-            
+            string[] result = null;
             string Path = "ConsoleApplication1.exe";
-            FileLoad f = new FileLoad(Path);
-            
+            //FileLoad f = new FileLoad(Path);
+            file f = new file(Path);
+            f.ShowAdres = true;
+            f.Minbyte = 0;
+            f.Maxbyte = 64;
+            f.WordSize = 2;
+            f.ColonWord = 4;
+           result =  f.dataFormat();
+            if (result != null)
+            {
+                for (int i = 0; i < result.Length; i++)
+                {
+                    Console.WriteLine(result[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("datu nav");
+            }
         }
     }
 }
--- Hex Bin/file.cs
+++ Hex Bin/file.cs
@@ -8,20 +8,16 @@ namespace Hex_Bin
 {
     internal class file
     {
-        private Dictionary<string, int> index = new Dictionary<string, int>()
-        {
-            ["db"] = 1,
-            ["dw"] = 2,
-            ["lw"] = 4
-        };
         //____________________________Parametri____________________________________________
         public string format = "hex"; // pieņem   hex bin dec asci  kada formata attēlot datus
         public char symbol = '.';     // Ja kods neatbilst nevienam simbolam tad to aizstāj ar šo simbolu
         public bool ShowAdres = false; //Parādīt rindu adreses
-        public string WordSize = null;       // No cik baitiem sastāvēs vards iespējamais 1 2 4
-        public int MinAdress = 0;      // no kuras baita sak lasit 
-        public int MaxAdress = 1024;   // lidz kuram baitam lasit
-        public string ColonWord = null;
+        public int WordSize = 1;       // No cik baitiem sastāvēs vards iespējamais 1 2 4
+        public int Minbyte = 0;      // no kuras baita sak lasit 
+        public int Maxbyte = 64;   // lidz kuram baitam lasit
+        private int minAdress = 0;
+        private int maxAdress = 0;
+        public int ColonWord = 1;
          //_________________________________________________________________________________
         private byte[] data = null;
         private string[] formatdata = null;
@@ -33,6 +29,8 @@ namespace Hex_Bin
         public byte[] Data => data;
         //___________________________Paligdati________________________________________________
         private string[] adressAray = null;   //sagatavotas rindam adreses atsevišķā metodē
+        private string[] asciiAray = null;
+        private string[] value = null;
         //_____________________________________________________________________________________
         public file(string path)     // konstruktors ja lasa no faila
         {
@@ -49,15 +47,49 @@ namespace Hex_Bin
             this.data = data;
         }
         //______________________Datu formatēšana___________________________________
+        public string[] DataFormatHex()
+        {
+            return formatdata;
+        }
         //______________________adrešu formatēšana__________________________________
         private void adressformat()
         {
-            //int adressArayLenght = (MaxAdress - MinAdress) / (index.)  
-            //adressAray = new string[adressArayLenght];
+            if (ShowAdres)
+            {
+                int line = Maxbyte - Minbyte; // apreikina cik bus linijas
+                line = line / (WordSize * ColonWord);
+                if ((Maxbyte - Minbyte) % (ColonWord * WordSize) != 0) // ja dalot ir atlikums tad pievieno vel vienu liniju
+                {
+                    line++;
+                }
+                adressAray = new string[line];  // izveido masivu liniju adresu izveidei
+                string acumulator;
+                for (int i = 0; i < line; i++)
+                {
+                    acumulator = Convert.ToString(Minbyte + (ColonWord * WordSize * i));
+                    while (acumulator.Length < 8)
+                    {
+                        acumulator = '0' + acumulator;
+                    }
+                    adressAray[i] = acumulator;
+                    acumulator = "";
+                }
+            }
         }
-        public void Format()
+        private void asciFormat()
         {
 //
+        }
+        private void valueFornat()
+        {
+//
+        }
+        public string[] dataFormat()
+        {
+            adressformat();
+            asciFormat();
+            valueFornat();
+            return formatdata;
         }
      }
 }
